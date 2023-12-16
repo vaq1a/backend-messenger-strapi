a849371fccb0d8e57dd7619b66886995aeb1f3c1
vadsirisko <vadim.sirisko@onilab.com>
2023-12-16 13:41:32 +0300

    feat: initial commit

---
+++ config/plugins.ts
@@ -0,0 +1,31 @@
+export default ({ env }) => ({
+    "strapi-plugin-populate-deep": {
+        config: {
+            defaultDepth: 5,
+        },
+    },
+    io: {
+        enabled: true,
+        config: {
+            IOServerOptions: {
+                cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+            },
+            contentTypes: {
+                message: "*",
+                chat: ["create"],
+            },
+            events: [
+                {
+                    name: "connection",
+                    handler: ({ strapi }, socket) => {
+                        strapi.log.info(`[io] new connection with id ${socket.id}`);
+
+                        socket.on("client-message", async (messageData) => {
+                            strapi.$io.raw("server-message", messageData);
+                        });
+                    },
+                },
+            ],
+        },
+    },
+});
